aa940bb801860f1a8d754c678d18c56b8a67810f
yashsojitra97 <yashsojitra97@gmail.com>
2024-09-12 22:06:42 +0530

    added new endpoint for info

--- api/handlers/taskHandler.go
+++ api/handlers/taskHandler.go
@@ -10,6 +10,11 @@ import (
 	"github.com/yash-sojitra/todo/services"
 )
 
+type PathInfo struct {
+	Path string `json:"path"`
+	Description string `json:"description"`
+}
+
 type taskHandler struct {
 	taskService services.TaskService
 }
@@ -20,11 +25,52 @@ func NewTaskHandler(taskService services.TaskService) *taskHandler {
 
 func (h *taskHandler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/hello", h.Hello).Methods("GET")
+	router.HandleFunc("/info", h.Info).Methods("GET")
 	router.HandleFunc("/tasks", h.GetAllTasks).Methods("GET")
 	router.HandleFunc("/tasks", h.CreateTask).Methods("POST")
 	router.HandleFunc("/tasks/{id}", h.GetTaskByID).Methods("GET")
 	router.HandleFunc("/tasks/{id}", h.UpdateTask).Methods("PUT")
 	router.HandleFunc("/tasks/{id}", h.DeleteTask).Methods("DELETE")
+}
+
+func (h *taskHandler) Info(w http.ResponseWriter, r *http.Request) {
+
+	JSONResponse := []PathInfo {
+		{
+			Path: "GET http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/hello",
+			Description: "gives hello world as a message in response",
+		},
+		{
+			Path: "GET http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/tasks",
+			Description: "gives all tasks present in the database.",
+		},
+		{
+			Path: "GET http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/tasks/1",
+			Description: "gives all task whose ID is 1",
+		},
+		{
+			Path: "POST http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/tasks",
+			Description: "creates task with following body {\"title\": \"api todo\",\"description\": \"add auth\"}",
+		},
+		{
+			Path: "DELETE http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/tasks/1",
+			Description: "deletes task specified by id",
+		},
+		{
+			Path: "PUT http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/tasks/1",
+			Description: "Updates the task with new given task to the task mentioned by id",
+		},
+		{
+			Path: "GET http://ec2-43-204-97-110.ap-south-1.compute.amazonaws.com/api/v1/info",
+			Description: "Gives all these info",
+		},
+	}
+
+
+	if err := utils.WriteJSON(w,http.StatusOK,&JSONResponse); err != nil {
+		utils.WriteError(w, http.StatusBadRequest,err)
+		return
+	}
 }
 
 func (h *taskHandler) Hello(w http.ResponseWriter, r *http.Request) {
